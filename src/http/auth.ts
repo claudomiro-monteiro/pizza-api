95f9f44fb659c22331de3d49912c8d97c13daf64
claudomiro <claudomiromonteiro@gmail.com>
2024-03-21 16:53:22 -0300

    :rocket: add token

--- src/http/auth.ts
+++ src/http/auth.ts
@@ -2,16 +2,13 @@ import { Elysia, t } from 'elysia'
 import { env } from '../env'
 
 import jwt from '@elysiajs/jwt'
-import cookie from '@elysiajs/cookie'
 
-export const auth = new Elysia()
-  .use(
-    jwt({
-      secret: env.JWT_SECRET_KEY,
-      schema: t.Object({
-        sub: t.String(),
-        restaurantId: t.Optional(t.String()),
-      }),
+export const auth = new Elysia().use(
+  jwt({
+    secret: env.JWT_SECRET_KEY,
+    schema: t.Object({
+      sub: t.String(),
+      restaurantId: t.Optional(t.String()),
     }),
-  )
-  .use(cookie())
+  }),
+)
